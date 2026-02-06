e719941fd13b49fb8593218bd5fa5512d7ce0216
Anshul Bhardwaj <70947798+BhardwajAnshul@users.noreply.github.com>
2026-02-07 00:39:57 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (17.50%) - LeetSync

---
+++ 48-rotate-image/rotate-image.py
@@ -0,0 +1,20 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        size = len(matrix)
+
+        done = set()
+        for j in range(size):
+            for i in range(size):
+                if f"{i}-{j}" in done:
+                    continue
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+                done.add(f"{i}-{j}")
+                done.add(f"{j}-{i}")
+
+        for j in range(size):
+            for i in range(size):
+                if i < size//2:
+                    matrix[j][i], matrix[j][size-1-i] = matrix[j][size-1-i], matrix[j][i]
+
+
+        
