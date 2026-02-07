91fb169412209fa321f0e9e3d58f52c5c7feaf89
Anshul Bhardwaj <70947798+BhardwajAnshul@users.noreply.github.com>
2026-02-07 23:05:51 +0530

    Time: 7 ms (30.88%) | Memory: 21.2 MB (37.76%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,13 +1,11 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
 
-        numbers = set()
-
-        for n in nums:
-            if n not in numbers:
-                numbers.add(n)
-            else:
-                numbers.remove(n)
-
-        return list(numbers)[0]
+        if len(nums)==1:
+            return nums[0]
+        
+        start = nums[0]
+        for num in nums[1:]:
+            start = start ^ num
+        return start
         
